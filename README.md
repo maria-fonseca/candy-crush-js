a5b2c7ffda159224291fe10591a621ee5b4a750f
Maria  Fonseca <maria@Marias-Mac-mini.local>
2021-02-28 22:18:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Candy Crush JS
+
+Description
+
+### How to run it locally
+
+1. `npm install`
+2. `npm run dev`
---
+++ app.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const width = 8;
+    const squares = [];
+
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+    //Create board 
+
+    function createBoard() {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div');
+            square.setAttribute('draggable', true);
+            square.setAttribute('id', i)
+            let randomColor = Math.floor(Math.random() * candyColors.length);
+            square.style.backgroundColor = candyColors[randomColor];
+            grid.appendChild(square);
+            squares.push(square)
+        }
+    }
+    
+    createBoard() 
+
+    //Drag the candies
+
+    let colorBeingDragged;
+    let colorBeingReplaced;
+    let squareIdBeingDragged;
+    let squareIdBeingReplaced;
+
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragEnd))
+    squares.forEach(square => square.addEventListener('dragover', dragOver))
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+    //11:14 mins pick back up
+    function dragStart() {
+        colorBeingDragged = this.style.backgroundColor;
+        squareIdBeingDragged = parseInt(this.id)
+        console.log(colorBeingDragged)
+        console.log(this.id, 'dragStart')       
+    }
+
+    function dragOver() {
+         console.log(this.id, 'dragOver')
+    }
+
+    function dragEnter() {
+         console.log(this.id, 'dragEnter')
+    }
+
+    function dragLeave() {
+         console.log(this.id, 'dragLeave')
+    }
+
+    function dragEnd() {
+         console.log(this.id, 'dragEnd')
+    }
+
+    function dragDrop() {
+         console.log(this.id, 'dragDrop')
+         colorBeingReplaced = this.style.backgroundColor;
+         squareIdBeingReplaced = parseInt(this.id)
+    }
+
+
+
+})
+
