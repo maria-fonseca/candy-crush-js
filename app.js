3092f91edc812dc464cbe39f32fb73a528319ac6
Maria <maria.amartinsfonseca@gmail.com>
2021-03-03 18:44:54 +0000

    Added checking for matches and valid moves

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8;
     const squares = [];
+    let score = 0;
 
     const candyColors = [
         'red',
@@ -42,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     squares.forEach(square => square.addEventListener('dragleave', dragLeave))
     squares.forEach(square => square.addEventListener('drop', dragDrop))
 
-    //11:14 mins pick back up
+
     function dragStart() {
         colorBeingDragged = this.style.backgroundColor;
         squareIdBeingDragged = parseInt(this.id)
@@ -50,11 +51,13 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(this.id, 'dragStart')       
     }
 
-    function dragOver() {
+    function dragOver(e) {
+        e.preventDefault()
          console.log(this.id, 'dragOver')
     }
 
-    function dragEnter() {
+    function dragEnter(e) {
+        e.preventDefault()
          console.log(this.id, 'dragEnter')
     }
 
@@ -64,15 +67,78 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function dragEnd() {
          console.log(this.id, 'dragEnd')
+         //what is a valid move? 
+         let validMoves = [
+            squareIdBeingDragged -1, 
+            squareIdBeingDragged -width,
+            squareIdBeingDragged +1,
+            squareIdBeingDragged +width, 
+            ];
+         let validMove = validMoves.includes(squareIdBeingReplaced);
+
+         if( squareIdBeingReplaced && validMove) {
+             squareIdBeingReplaced = null;
+         } else if (squareIdBeingReplaced && !validMove) {
+             squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced;
+             squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+         } else {
+             squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+         }
+
     }
 
     function dragDrop() {
          console.log(this.id, 'dragDrop')
          colorBeingReplaced = this.style.backgroundColor;
          squareIdBeingReplaced = parseInt(this.id)
+         this.style.backgroundColor = colorBeingDragged;
+         squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
     }
 
+    //Checking for matches
+    //check for row of three
+    function checkRowForThree() {
+        for (i = 0; i < 61; i++) {
+            let rowOfThree = [i, i+1, i+2];
+            let decidedColor = squares[i].style.backgroundColor;
+            const isBlank = squares[i].style.backgroundColor === '';
 
+            if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3;
+                rowOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = '';
+                })
+            }
+
+        }
+    }
+    checkRowForThree()
+
+    //check for column of three
+    function checkColumnForThree() {
+        for (i = 0; i < 47; i++) {
+            let columnOfThree = [i, i+width, i+width*2];
+            let decidedColor = squares[i].style.backgroundColor;
+            const isBlank = squares[i].style.backgroundColor === '';
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+
+            if(notValid.includes(i)) continue
+
+            if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3;
+                columnOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = '';
+                })
+            }
+
+        }
+    }
+    checkColumnForThree()
+
+    window.setInterval(function(){
+        checkRowForThree()
+        checkColumnForThree()
+    }, 100)
 
 })
 
